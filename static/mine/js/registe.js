cda0702d0c43f2574116dd907a115f7bb94a612d
zyz <zyz@1000phone.com>
2018-09-29 09:56:20 +0800

    正则表达式验证

--- static/mine/js/registe.js
+++ static/mine/js/registe.js
@@ -32,4 +32,22 @@ $(function () {
             $('#mypassworderr').hide()
         }
     })
+
+
+    // 手机号码校验
+    var reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+    // 默认图标是隐藏
+    $('.tel-view>.glyphicon').hide()
+    $('#tel').blur(function () {
+        var flag = reg.test($(this).val())
+        if (flag){  // 通过
+            $('.tel-view').removeClass('has-error').addClass('has-success')
+            $('.tel-view>.glyphicon').show().removeClass('glyphicon-remove').addClass('glyphicon-ok')
+            $('#subButton').removeAttr('disabled')
+        } else {    // 不通过
+            $('.tel-view').removeClass('has-success').addClass('has-error')
+            $('.tel-view>.glyphicon').show().removeClass('glyphicon-ok').addClass('glyphicon-remove')
+            $('#subButton').attr('disabled','disabled')
+        }
+    })
 })
